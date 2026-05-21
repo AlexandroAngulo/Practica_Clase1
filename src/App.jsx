8b0c0cbbba79e7745aa4339a49649bc1307f5a71
AlexandroAngulo <152337375+AlexandroAngulo@users.noreply.github.com>
2026-05-21 11:56:33 -0600

    Updated API_URL to use environment variable for better configuration management

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Admin from './views/Admin'
 import ResponsiveAppBar from './components/AppBar'
 import { Container } from '@mui/material'
 
-const API_URL = "http://localhost:8000";
+const API_URL = process.env.REACT_APP_API_URL || "api-66-production.up.railway.app" || "http://localhost:8000";
 
 function App() {
   const [isLogin, setIsLogin] = useState(() => {
